9e45b8e5d9d2fbe1a2ec7f5e0cc1248de02177b5
ramywael <ramybakry9@gmail.com>
2023-02-20 11:54:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Description: The main function prints a message to the console.
+ *Description: 'the program's description'
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    /* main function implementation goes here */
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-
-    /* This program returns 0 if it completes successfully */
-    return (0);
+	/* main function implementation goes here*/
+	puts("\"Programming is like building a multilingual puzzle");
+	/*This program returns 0 if it completes successfully*/
+	return (0);
 }

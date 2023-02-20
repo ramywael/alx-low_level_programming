1f63809988df86c11b815181777d7d07e3d3e6c6
ramywael <ramybakry9@gmail.com>
2023-02-20 11:59:33 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,4 +6,10 @@
  *
  *Return: Always 0 (Success)
  */
-int main(voi
+int main(void)
+{
+	/* main function implementation goes here*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*This program returns 0 if it completes successfully*/
+	return (0);
+}

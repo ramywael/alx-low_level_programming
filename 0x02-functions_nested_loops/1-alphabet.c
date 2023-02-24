d7dcb452fb0512d616a209dcba39427351ad7afa
ramywael <ramybakry9@gmail.com>
2023-02-23 16:03:16 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+for (char i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
